af107b4492c440721c371157fd104672369bd0c4
Webber <166926057+webber3242@users.noreply.github.com>
2025-05-17 12:30:20 -0700

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewApps = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonToggleStatus = new System.Windows.Forms.Button();
+            this.buttonBulkAdd = new System.Windows.Forms.Button();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+
+            // For label2 (seconds text)
+
+            // For groupBox1 (container)
+            // Set form background
+            this.BackColor = Color.FromArgb(30, 31, 41);
+
+            // Style the numeric up/down control
+            this.statusStrip1.SuspendLayout();
+
+
+            // 
+            // listViewApps
+            // 
+            this.listViewApps.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewApps.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewApps.FullRowSelect = true;
+            this.listViewApps.HideSelection = false;
+            this.listViewApps.Location = new System.Drawing.Point(12, 42);
+            this.listViewApps.Name = "listViewApps";
+            this.listViewApps.Size = new System.Drawing.Size(600, 250);
+            this.listViewApps.TabIndex = 0;
+            this.listViewApps.UseCompatibleStateImageBehavior = false;
+            this.listViewApps.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Application";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 1400;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 110;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAdd.Location = new System.Drawing.Point(12, 298);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(80, 30);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonRemove.Location = new System.Drawing.Point(98, 298);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(80, 30);
+            this.buttonRemove.TabIndex = 2;
+            this.buttonRemove.Text = "remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonEdit.Location = new System.Drawing.Point(184, 298);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(80, 30);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.Text = "edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonToggleStatus
+            // 
+            this.buttonToggleStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonToggleStatus.Location = new System.Drawing.Point(270, 298);
+            this.buttonToggleStatus.Name = "buttonToggleStatus";
+            this.buttonToggleStatus.Size = new System.Drawing.Size(100, 30);
+            this.buttonToggleStatus.TabIndex = 4;
+            this.buttonToggleStatus.Text = "enab/disb";
+            this.buttonToggleStatus.UseVisualStyleBackColor = true;
+            this.buttonToggleStatus.Click += new System.EventHandler(this.buttonToggleStatus_Click);
+            // 
+            // buttonBulkAdd
+            // 
+            this.buttonBulkAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonBulkAdd.Location = new System.Drawing.Point(376, 298);
+            this.buttonBulkAdd.Name = "buttonBulkAdd";
+            this.buttonBulkAdd.Size = new System.Drawing.Size(80, 30);
+            this.buttonBulkAdd.TabIndex = 5;
+            this.buttonBulkAdd.Text = "add by path";
+            this.buttonBulkAdd.UseVisualStyleBackColor = true;
+            this.buttonBulkAdd.Click += new System.EventHandler(this.buttonBulkAdd_Click);
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStartStop.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.buttonStartStop.Location = new System.Drawing.Point(462, 298);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(150, 40);
+            this.buttonStartStop.TabIndex = 6;
+            this.buttonStartStop.Text = "Run applications";
+            this.buttonStartStop.UseVisualStyleBackColor = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 409);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Ready";
+            // 
+            // label1
+            // 
+
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(37)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(624, 431);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonStartStop);
+            this.Controls.Add(this.buttonToggleStatus);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonBulkAdd);
+            this.Controls.Add(this.listViewApps);
+            this.MinimumSize = new System.Drawing.Size(624, 431);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".exe.ps1.bat.ahk Launcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        #endregion
+
+        private System.Windows.Forms.ListView listViewApps;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonToggleStatus;
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Button buttonBulkAdd;
+        private NumericUpDown numericDelay;
+        private GroupBox groupBox1;
+    }
+}
